1504246d0900db950a5065f43fce964bf4adceda
Jan Krukowski <jan.krukowski@geneva.com>
2025-11-20 09:27:30 +0100

    update SQLite to 3510000

--- deps.sh
+++ deps.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+# Configurable versions
+SQLITE_VERSION=3510000
+SQLITE_YEAR=2025
+SQLITE_VEC_VERSION=0.1.6
+
 # download sqlite amalgamation
 
-curl -o sqlite-amalgamation.zip https://www.sqlite.org/2024/sqlite-amalgamation-3470000.zip
-unzip sqlite-amalgamation.zip
-cp sqlite-amalgamation-3470000/sqlite3.c Sources/CSQLiteVec/
-cp sqlite-amalgamation-3470000/sqlite3.h Sources/CSQLiteVec/include/
-cp sqlite-amalgamation-3470000/sqlite3ext.h Sources/CSQLiteVec/include/
-rm -rf sqlite-amalgamation-3470000
+curl -o sqlite-amalgamation.zip https://www.sqlite.org/${SQLITE_YEAR}/sqlite-amalgamation-${SQLITE_VERSION}.zip
+unzip -q sqlite-amalgamation.zip
+SQLITE_DIR=$(unzip -l sqlite-amalgamation.zip | grep -m1 "sqlite-amalgamation-" | awk '{print $4}' | cut -d/ -f1)
+cp ${SQLITE_DIR}/sqlite3.c Sources/CSQLiteVec/
+cp ${SQLITE_DIR}/sqlite3.h Sources/CSQLiteVec/include/
+cp ${SQLITE_DIR}/sqlite3ext.h Sources/CSQLiteVec/include/
+rm -rf ${SQLITE_DIR}
 rm sqlite-amalgamation.zip
 
 # download sqlite-vec amalgamation
 
-curl -o sqlite-vec-amalgamation.zip -L https://github.com/asg017/sqlite-vec/releases/download/v0.1.6/sqlite-vec-0.1.6-amalgamation.zip
-unzip sqlite-vec-amalgamation.zip
+curl -o sqlite-vec-amalgamation.zip -L https://github.com/asg017/sqlite-vec/releases/download/v${SQLITE_VEC_VERSION}/sqlite-vec-${SQLITE_VEC_VERSION}-amalgamation.zip
+unzip -q sqlite-vec-amalgamation.zip
 mv sqlite-vec.c Sources/CSQLiteVec/
 mv sqlite-vec.h Sources/CSQLiteVec/include/
 rm sqlite-vec-amalgamation.zip
